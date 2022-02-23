327e4e24ef54ce5ee1dd7bc03071230274bc3b38
rcmdesenvolvimento <rcmdesenvolvimento@gmail.com>
2022-02-23 10:13:44 -0300

    commit incial

---
+++ src/main/java/com/ricardo/dev/miranda/DemoSpringGitApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.dev.miranda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/dev/miranda/DemoSpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.dev.miranda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
